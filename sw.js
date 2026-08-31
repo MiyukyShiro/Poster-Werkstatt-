019d5be89ede23d158dee948eb407e91296ec7bd
MiyukyShiro <tschui.silvan@protonmail.com>
2026-08-31 08:28:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Poster-Werkstatt — offline verfügbar, aber Updates kommen sofort an. */
-const CACHE = "poster-werkstatt-v3";
+const CACHE = "poster-werkstatt-v8";
 const FILES = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", e => {
